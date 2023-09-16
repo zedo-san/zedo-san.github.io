baa3149318237648ed8360e4bc09808bdb6f9eeb
Zedfeld Busiños <zedfeld.businos@gmail.com>
2023-09-16 17:45:48 +0800

    style: adds detailed description for projects and improves responsiveness

---
+++ src/components/RedirectBadge/RedirectBadge.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  redirectLink: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <a
+    class="badge badge-outline hover:bg-black hover:text-white rounded-full p-3"
+    :href="props.redirectLink"
+    target="_blank"
+    rel="noreferrer"
+  >
+    {{ props.redirectLink }}
+  </a>
+</template>
